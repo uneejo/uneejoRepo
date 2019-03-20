868119827ebc0e495d405b698df584fd7b553ed9
unee <mbest1212@naver.com>
2019-03-20 17:21:58 +0900

    t3.java 추가함

---
+++ t.java
@@ -0,0 +1,20 @@
+class Child{
+       String name = "ȫ�浿";
+       int grade = 5;
+       School school = new School ();
+}
+class School{
+      String address;
+      String name;
+}
+class A {
+ public static void main(String[] args){
+
+
+
+
+System.out.println(school1.name);
+System.out.println(school2.name);
+
+}
+}
